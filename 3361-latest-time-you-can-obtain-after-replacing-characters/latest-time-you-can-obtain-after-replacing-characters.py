29ce887c976c3ad5f70150a96026552a2a0e133d
alan <114007485+alanthoms@users.noreply.github.com>
2025-10-06 17:37:52 +0100

    Time: 42 ms (42.65%) | Memory: 18 MB (8.33%) - LeetSync

--- 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.py
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findLatestTime(self, s:str) -> str:
+    def findLatestTime(self, s):
         hr0, hr1, _, mn0, mn1 = list(s)
         if hr0 == "?": hr0 = "1" if hr1 in "?01" else "0"
         if hr1 == "?": hr1 = "1" if hr0 == "1" else "9"
